eaac055bff1bbb11a3472c4bb843ee370405ad66
yokkit <yoko.tochigi@gmail.com>
2022-01-09 18:42:57 +0100

    try

---
+++ test.py
@@ -0,0 +1,12 @@
+import folium
+import json
+import pandas as pd
+import numpy as np
+import streamlit as st
+from streamlit_folium import folium_static
+
+chiba_city = [35.609985,140.118126]
+m = folium.Map(location=chiba_city, tiles='cartodbpositron', zoom_start=8)
+st.header('テスト的にアップしたアプリです')
+st.subheader('千葉県周辺のマップです')
+folium_static(m)
